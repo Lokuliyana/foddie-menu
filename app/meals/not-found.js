262b96f1022dc13befd9a1ad6bc4fc6e8460a41b
Lokuliyana <lokuliyanacp@gmail.com>
2025-03-06 22:35:41 +0530

    Final project

---
+++ app/meals/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFOund () {
+  return (
+    <main className="not-found">
+      <h1>Meal Not Found</h1>
+      <p>Sorry not found the meal you want</p>
+    </main>
+  )
+}
---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFOund () {
+  return (
+    <main className="not-found">
+      <h1>Not Found</h1>
+      <p>Sorry not found the page you want</p>
+    </main>
+  )
+}
